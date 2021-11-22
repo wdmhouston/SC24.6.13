c8c036453feb485498733ff70734186e4ccb3582
Deming Wang <wangdeming@gmail.com>
2021-11-22 01:57:44 -0600

    update predict

--- project/tests/predict.py
+++ project/tests/predict.py
@@ -2,5 +2,6 @@ import unittest
 from modules.model import model
 
 model1 = model()
-image_path = "/app/project/data/data3/apple/Image_1.jpg"
+image_path = "/app/project/data/test/apple/Image_1.jpg"
 print(model1.predict(image_path))
+
